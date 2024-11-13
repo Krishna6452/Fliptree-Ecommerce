1505238151514eaf51409709d4d663d30c320b17
Krishna6452 <krishnapriya6452@gmail.com>
2024-11-13 17:00:02 +0530

    Update ProductListing.js

--- src/component/ProductListing.js
+++ src/component/ProductListing.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
 
 function ProductListing({ products }) {
+  const { addToCart } = useContext(AppContext);
+
   return (
     <div className="product-listing">
       {products.map((product) => (
@@ -8,7 +11,7 @@ function ProductListing({ products }) {
           <img src={product.image} alt={product.name} />
           <h3>{product.name}</h3>
           <p>${product.price}</p>
-          <button>Add to Cart</button>
+          <button onClick={() => addToCart(product)}>Add to Cart</button>
         </div>
       ))}
     </div>
