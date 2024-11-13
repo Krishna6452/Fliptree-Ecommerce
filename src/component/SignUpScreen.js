6033b49ef28bdb35fa5ff7f15103584498945d6a
Krishna6452 <krishnapriya6452@gmail.com>
2024-11-13 16:47:05 +0530

    Create SignUpScreen.js

---
+++ src/component/SignUpScreen.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+function SignUpScreen() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignUp = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="signup-screen">
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSignUp}>
+        <input 
+          type="text" 
+          placeholder="Name" 
+          value={name} 
+          onChange={(e) => setName(e.target.value)} 
+          required 
+        />
+        <input 
+          type="email" 
+          placeholder="Email" 
+          value={email} 
+          onChange={(e) => setEmail(e.target.value)} 
+          required 
+        />
+        <input 
+          type="password" 
+          placeholder="Password" 
+          value={password} 
+          onChange={(e) => setPassword(e.target.value)} 
+          required 
+        />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+}
+
+export default SignUpScreen;
